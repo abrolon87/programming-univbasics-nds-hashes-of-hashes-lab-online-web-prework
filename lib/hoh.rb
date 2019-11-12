f8ea44a418712c713c6ee75faf4e23bb0525b6dc
Amanda Rolon <abrolon87@gmail.com>
2019-11-12 17:02:01 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,13 +32,14 @@
 
 def naming_system
   @Kingdom = {
-      :sub_category=> {
+    :sub_category=> {
       :label=>"Phylum",
       :sub_category=> {
         :label=> "Class",
         :sub_category=> {
           :label=> "Order",
           :sub_category=> {
+            :label=> "Family",
             :sub_category=> {
               :label=> "Genus",
               :sub_category=> {
