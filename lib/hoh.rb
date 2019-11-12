4bce73f254877f703428b148abeb9e73bd92a0fc
Amanda Rolon <abrolon87@gmail.com>
2019-11-12 16:39:58 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -2,29 +2,29 @@
 # on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
 # that HoH as the return value for the method "naming_system."
 #
-# The tests will guide your construction.
+#The tests will guide your construction.
 #
 # PORTION_1 = {
 #   label: "Kingdom",
 #   sub_category: {
-#     label: "Phylum",
+#   label: "Phylum",
 #     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-# 
+#     label: "Class"
+#    }
+#  }
+#}
+#
 # PORTION_2 = {
 #   label: "Order"
 # }
-# 
+#
 # PORTION_3 = {
 #   label: "Family",
 #   sub_category: {
 #     label: "Genus",
 #   }
 # }
-# 
+#
 # PORTION_4 = {
 #   label: "Species",
 #   sub_category: nil
